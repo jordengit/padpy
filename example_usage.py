630b7a0f90623d187a503621387b1b01e1b52991
tankorsmash <tankorsmash@gmail.com>
2015-05-18 14:59:49 -0400

    calc level xp in example

--- example_usage.py
+++ example_usage.py
@@ -6,12 +6,14 @@ from filters import MonsterFilter
 
 @argh.arg("monster_id", type=int, help="ID of the monster")
 def get_monster(monster_id, verbose=False):
+    """ outputs a monster's complete stats """
     padpy = Pad(verbose=verbose)
 
     monster = padpy.get_monster(monster_id)
     padpy.pretty_print(monster)
 
 def get_all_monsters(verbose=False):
+    """ outputs all monsters' complete stats """
     padpy = Pad(verbose=verbose)
 
     monsters = padpy.get_all_monsters()
@@ -19,8 +21,22 @@ def get_all_monsters(verbose=False):
         padpy.pretty_print(monster)
         print
 
+@argh.arg("monster_id", type=int, help="ID of the monster")
+@argh.arg("level", type=int, help="Target Level")
+def calc_xp(monster_id, level, verbose=False):
+    """ calculates XP required from level 1 to level """
+    padpy = Pad(verbose=verbose)
+
+    monster = padpy.get_monster(monster_id)
+    print "From level 1 to level %s" % level
+    print "%s XP is required" % monster.xp_curve.calc_for_level(level)
+
 parser = argh.ArghParser()
-parser.add_commands([get_monster, get_all_monsters])
+parser.add_commands([
+    get_monster,
+    get_all_monsters,
+    calc_xp,
+])
 
 if __name__ == "__main__":
     parser.dispatch()
