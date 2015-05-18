9743c40f1a3e5deb217d2b944775403afab95691
tankorsmash <tankorsmash@gmail.com>
2015-05-18 14:05:36 -0400

    trim down example_usage

--- example_usage.py
+++ example_usage.py
@@ -12,10 +12,4 @@ if __name__ == "__main__":
     monsters = padpy.get_all_monsters()
 
     for monster in monsters:
-        print 'Monster #%s is: %s' % (monster.id, monster)
-        ls = monster.leader_skill
-        print "\tLeader Skill", ls
-        ld = ls.data
-        print "\tLeader Data", ld
-        lc = ld.constraints
-        print "\tLeader Constraints", lc
+        print 'Monster %s' % monster
